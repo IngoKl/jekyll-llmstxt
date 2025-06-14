875c87b3bd3f838f8f10e3646ac7415e68b15951
Ingo Kleiber <ikleiber@googlemail.com>
2025-06-14 19:57:30 +0200

    Update jekyll-llmstxt.rb

--- lib/jekyll-llmstxt.rb
+++ lib/jekyll-llmstxt.rb
@@ -11,25 +11,26 @@ module Jekyll
         file.content = site.config["title"] ? "# #{site.config["title"]}\n\n" : ""
         file.content += site.config["description"] ? "#{site.config["description"]}\n\n" : ""
 
+        # List pages
+        if site.config["llmspages"].is_a?(Array) && !site.config["llmspages"].empty?
+          file.content += "## Pages\n\n"
+          site.config["llmspages"].each do |p|
+            title = p["title"] || p["url"]
+            url   = p["url"].to_s
+            # ensure trailing slash
+            url += "/" unless url.end_with?("/")
+            link = site.baseurl ? File.join(site.baseurl, url, "index.md") : File.join(url, "index.md")
+            file.content += "- [#{title}](#{site.config["url"]}#{link})\n"
+          end
+          file.content += "\n\n"
+        end
+
+
         # List posts
         file.content += "## Posts\n\n"
         site.posts.docs.each do |post|
           post_url = site.baseurl ? File.join(site.baseurl, post.url) : post.url
-          file.content += "- [#{post.data["title"]}](#{post_url}index.md)\n"
-        end
-
-        # List pages
-        file.content += "\n## Pages\n\n"
-        site.pages.each do |page|
-          # Skip the llms.txt itself
-          next if page.name == 'llms.txt'
-
-          # Use page title if set, otherwise fall back to the URL
-          title = page.data['title'] || page.url.sub(/^\//, '').sub(/\/$/, '').capitalize
-          page_url = site.baseurl ? File.join(site.baseurl, page.url) : page.url
-
-          # Link to the page’s index.md (if you’re copying pages to index.md via your hook)
-          file.content += "- [#{title}](#{page_url}index.md)\n"
+          file.content += "- [Title: #{post.data["title"]} Description: #{post.data["description"]}](#{site.config["url"]}#{post_url}index.md)\n"
         end
 
         file.data["layout"] = nil
@@ -64,10 +65,21 @@ Jekyll::Hooks.register :site, :post_write do |site|
 end
 
 Jekyll::Hooks.register :pages, :post_write do |page|
-  next if page.name == 'llms.txt'
+  config_pages = Array(page.site.config['llmspages'])
 
-  target_dir  = File.join(page.site.dest, page.url)
-  target_path = File.join(target_dir, "index.md")
-  FileUtils.mkdir_p(target_dir)
-  FileUtils.cp(page.path, target_path)
+  # see if this page's URL matches one of the entries
+  config_pages.each do |entry|
+    desired = entry['url'].to_s
+    desired << "/" unless desired.end_with?("/")
+
+    next unless page.url == desired
+
+    src = File.join(page.site.source, page.path)
+    next unless File.file?(src)
+
+    dest_dir = File.join(page.site.dest, desired)
+    FileUtils.mkdir_p(dest_dir)
+
+    FileUtils.cp(src, File.join(dest_dir, "index.md"))
+  end
 end
